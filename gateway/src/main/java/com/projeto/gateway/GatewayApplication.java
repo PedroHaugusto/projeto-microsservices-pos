3c8736a1e497efcfe2c065bf8b3819ceb0f4628c
Pedro Henrique <pedrohaugusto12@gmail.com>
2025-10-27 23:08:40 -0300

    feat: criando gateway

--- gateway/src/main/java/com/projeto/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/projeto/gateway/GatewayApplication.java
@@ -2,12 +2,24 @@ package com.projeto.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator router(RouterLocatorBuilder builder){
+		return builder.routes()
+				.route(r -> r.path("/produtos/**").uri("lb://msprodutos"))
+				.route(r -> r.path("/pedidos/**").uri("lb://mspedidos"))
+				.build();
+	}
 }
